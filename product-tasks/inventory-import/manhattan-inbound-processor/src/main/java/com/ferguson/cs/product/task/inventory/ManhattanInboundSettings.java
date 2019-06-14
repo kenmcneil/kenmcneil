a38b5ff0772650938c0972bd615989302ca66904
Shawn Schafer <shawn.schafer@build.com>
2019-06-14 16:47:52 -0700

    SODEV-16743 - fix: Cleanup and task json file

--- product-tasks/inventory-import/manhattan-inbound-processor/src/main/java/com/ferguson/cs/product/task/inventory/ManhattanInboundSettings.java
+++ product-tasks/inventory-import/manhattan-inbound-processor/src/main/java/com/ferguson/cs/product/task/inventory/ManhattanInboundSettings.java
@@ -1,24 +1,13 @@
 package com.ferguson.cs.product.task.inventory;
 
-import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties("inventory-import.manhattan")
 public class ManhattanInboundSettings {
-	private String manhattanInputFilePath;
 	private String manhattanOutputFilePath;
-	private Map<String, String> locationIdDcMap;
 	private Long jobCompletionTimeOutInMilliseconds;
-
-	public String getManhattanInputFilePath() {
-		return manhattanInputFilePath;
-	}
-
-	public void setManhattanInputFilePath(String manhattanInputFilePath) {
-		this.manhattanInputFilePath = manhattanInputFilePath;
-	}
 
 	public String getManhattanOutputFilePath() {
 		return manhattanOutputFilePath;
@@ -26,14 +15,6 @@ public class ManhattanInboundSettings {
 
 	public void setManhattanOutputFilePath(String manhattanOutputFilePath) {
 		this.manhattanOutputFilePath = manhattanOutputFilePath;
-	}
-
-	public Map<String, String> getLocationIdDcMap() {
-		return locationIdDcMap;
-	}
-
-	public void setLocationIdDcMap(Map<String, String> locationIdDcMap) {
-		this.locationIdDcMap = locationIdDcMap;
 	}
 
 	public Long getJobCompletionTimeOutInMilliseconds() {
