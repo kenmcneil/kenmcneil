91597b788aadb3888ea69f420a1595845790f9e7
Jason Conkey <jason.conkey@build.com>
2020-04-06 11:13:11 -0700

    SODEV-24837 - chore: update logging output

--- product-streams/product-participation-engine/src/main/java/com/ferguson/cs/product/stream/participation/engine/ParticipationServiceImpl.java
+++ product-streams/product-participation-engine/src/main/java/com/ferguson/cs/product/stream/participation/engine/ParticipationServiceImpl.java
@@ -31,47 +31,47 @@ public class ParticipationServiceImpl implements ParticipationService {
 
 		int rowsAffected = participationDao.setParticipationIsActive(participationId, true);
 		totalRows += rowsAffected;
-		LOG.trace("==== activating participation " + participationId + " ====");
+		LOG.debug("==== activating participation " + participationId + " ====");
 
 		// determine what products are changing ownership and store into temp table
 		rowsAffected = participationDao.updateOwnerChangesForActivation(participationId);
 		totalRows += rowsAffected;
-		LOG.trace(participationId + ": updating " + rowsAffected + " products for activation");
+		LOG.debug(participationId + ": updating " + rowsAffected + " products for activation");
 
 		rowsAffected = participationDao.addProductOwnershipForNewOwners(participationId);
-		LOG.trace(participationId + ": " + rowsAffected + " products with new participation ownership");
+		LOG.debug(participationId + ": " + rowsAffected + " products with new participation ownership");
 		totalRows += rowsAffected;
 
 		rowsAffected = participationDao.removeProductOwnershipForOldOwners(participationId);
-		LOG.trace(participationId + ": " + rowsAffected + " products dis-owned from other participations");
+		LOG.debug(participationId + ": " + rowsAffected + " products dis-owned from other participations");
 		totalRows += rowsAffected;
 
 		rowsAffected = participationDao.updateProductSaleIds(participationId);
-		LOG.trace(participationId + ": " + rowsAffected + " product sale ids updated");
+		LOG.debug(participationId + ": " + rowsAffected + " product sale ids updated");
 		totalRows += rowsAffected;
 
 		rowsAffected = participationDao.updateLastOnSaleBasePrices(processingDate);
-		LOG.trace(participationId + ": " + rowsAffected + " lastOnSale basePrice values saved");
+		LOG.debug(participationId + ": " + rowsAffected + " lastOnSale basePrice values saved");
 		totalRows += rowsAffected;
 
 		rowsAffected = participationDao.takePricesOffSaleAndApplyPendingBasePriceUpdates(userId);
-		LOG.trace(participationId + ": " + rowsAffected + " prices taken off sale from calculated discounts");
+		LOG.debug(participationId + ": " + rowsAffected + " prices taken off sale from calculated discounts");
 		totalRows += rowsAffected;
 
 		// activate new discounts (if any)
 		rowsAffected = participationDao.applyNewCalculatedDiscounts(processingDate, userId);
-		LOG.trace(participationId + ": " + rowsAffected + " prices put on sale from calculated discounts");
+		LOG.debug(participationId + ": " + rowsAffected + " prices put on sale from calculated discounts");
 		totalRows += rowsAffected;
 
 		// update modified date on each product modified
 		rowsAffected = participationDao.updateProductModifiedDates(processingDate, userId);
-		LOG.trace(participationId + ": " + rowsAffected + " product modified dates updated");
+		LOG.debug(participationId + ": " + rowsAffected + " product modified dates updated");
 		totalRows += rowsAffected;
 
 		// TODO remove currentPriorityParticipation code
 		participationDao.syncToCurrentPriorityParticipation();
 
-		LOG.trace(participationId + ": " + totalRows + " total rows updated to activate");
+		LOG.debug(participationId + ": " + totalRows + " total rows updated to activate");
 	}
 
 	@Transactional
@@ -82,52 +82,52 @@ public class ParticipationServiceImpl implements ParticipationService {
 		int totalRows = 0;
 
 		int rowsAffected = participationDao.setParticipationIsActive(participationId, false);
-		LOG.trace("==== deactivating participation " + participationId + " ====");
+		LOG.debug("==== deactivating participation " + participationId + " ====");
 		totalRows += rowsAffected;
 
 		// determine what products are changing ownership and store into temp table
 		rowsAffected = participationDao.updateOwnerChangesForDeactivation(participationId);
-		LOG.trace(participationId + ": updating " + rowsAffected + " products for deactivation");
+		LOG.debug(participationId + ": updating " + rowsAffected + " products for deactivation");
 		totalRows += rowsAffected;
 
 		// assign ownership of each unique id to any active fallback participations,
 		// but don't bother to disown from deactivating participation since it will be deleted at the end
 		rowsAffected = participationDao.addProductOwnershipForNewOwners(participationId);
-		LOG.trace(participationId + ": " + rowsAffected + " products with new participation ownership");
+		LOG.debug(participationId + ": " + rowsAffected + " products with new participation ownership");
 		totalRows += rowsAffected;
 
 		// update sale ids to fallback participations or to zeros
 		rowsAffected = participationDao.updateProductSaleIds(participationId);
-		LOG.trace(participationId + ": " + rowsAffected + " product sale ids updated");
+		LOG.debug(participationId + ": " + rowsAffected + " product sale ids updated");
 		totalRows += rowsAffected;
 
 		rowsAffected = participationDao.updateLastOnSaleBasePrices(processingDate);
-		LOG.trace(participationId + ": " + rowsAffected + " lastOnSale basePrice values saved");
+		LOG.debug(participationId + ": " + rowsAffected + " lastOnSale basePrice values saved");
 		totalRows += rowsAffected;
 
 		rowsAffected = participationDao.takePricesOffSaleAndApplyPendingBasePriceUpdates(userId);
-		LOG.trace(participationId + ": " + rowsAffected + " prices taken off sale from calculated discounts");
+		LOG.debug(participationId + ": " + rowsAffected + " prices taken off sale from calculated discounts");
 		totalRows += rowsAffected;
 
 		// activate fallback discounts (if any)
 		rowsAffected = participationDao.applyNewCalculatedDiscounts(processingDate, userId);
-		LOG.trace(participationId + ": " + rowsAffected + " prices put on sale from calculated discounts");
+		LOG.debug(participationId + ": " + rowsAffected + " prices put on sale from calculated discounts");
 		totalRows += rowsAffected;
 
 		// update modified date on each product modified
 		rowsAffected = participationDao.updateProductModifiedDates(processingDate, userId);
-		LOG.trace(participationId + ": " + rowsAffected + " product modified dates updated");
+		LOG.debug(participationId + ": " + rowsAffected + " product modified dates updated");
 		totalRows += rowsAffected;
 
 		// remove all records for this participation
 		rowsAffected = participationDao.deleteParticipation(participationId);
-		LOG.trace(participationId + ": " + rowsAffected + " rows removed to delete participation");
+		LOG.debug(participationId + ": " + rowsAffected + " rows removed to delete participation");
 		totalRows += rowsAffected;
 
 		// TODO remove currentPriorityParticipation code
 		participationDao.syncToCurrentPriorityParticipation();
 
-		LOG.trace(participationId + ": " + totalRows + " total rows updated to deactivate");
+		LOG.debug(participationId + ": " + totalRows + " total rows updated to deactivate");
 	}
 
 	@Transactional
@@ -141,7 +141,7 @@ public class ParticipationServiceImpl implements ParticipationService {
 		} else {
 			// only delete the participation records
 			int rowsAffected = participationDao.deleteParticipation(participationId);
-			LOG.trace(participationId + ": " + rowsAffected + " rows removed to delete participation");
+			LOG.debug(participationId + ": " + rowsAffected + " rows removed to delete participation");
 		}
 	}
 }
