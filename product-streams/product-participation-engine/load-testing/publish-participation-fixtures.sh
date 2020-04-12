24a3300b8f90182f707138fddc11038cf916c594
Jason Conkey <jason.conkey@build.com>
2020-04-11 23:27:30 -0700

    SODEV-24837 - feat: add docs

---
+++ product-streams/product-participation-engine/load-testing/publish-participation-fixtures.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# usage: ./publish-participation-fixtures.sh COUNT
+
+SERVICES_HOST=localhost:8080
+# SERVICES_HOST=webservices-dev-1.build.internal:8080
+
+USER_ID=2524 # jason
+
+START_ID=5000
+COUNT=$1
+
+for (( id = 5000; id < START_ID+COUNT; id++ ))
+do
+    echo -n "Publishing id $id ..."
+    curl -X PUT -H "Content-Type: application/json" http://$SERVICES_HOST/contents/participation/item/$id/publish?userId=$USER_ID
+    echo " done."
+done
