3e8b494cf93651f5589e4f6390785538cf24120a
Shawn Schafer <shawn.schafer@build.com>
2021-05-25 15:50:58 -0700

    SODEV-31389 - fix: Remove unused import/annotation

--- product-tasks/fei-omni-price-harmonization/src/main/java/com/ferguson/cs/product/task/omnipriceharmonization/OmniPriceHarmonizationApplication.java
+++ product-tasks/fei-omni-price-harmonization/src/main/java/com/ferguson/cs/product/task/omnipriceharmonization/OmniPriceHarmonizationApplication.java
@@ -2,10 +2,8 @@ package com.ferguson.cs.product.task.omnipriceharmonization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.ferguson.cs.product.task.omnipriceharmonization")
-@EnableFeignClients
 public class OmniPriceHarmonizationApplication {
 
 	public static void main(String args[]) {
